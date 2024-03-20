e3e967575cb4dfd01ff9b20f9c25aae1c44166c2
sirx2713 <bradx2713@gmail.com>
2024-03-20 04:05:04 +0300

    Initial Commit

---
+++ Passcode/PasscodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasscodeApp.swift
+//  Passcode
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct PasscodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
